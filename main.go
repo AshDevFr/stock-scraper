b81e94735ee702186b51f878c5118f2207432b06
Sylvain CAU <ashdevfr@gmail.com>
2020-12-20 19:59:08 -0800

    UI: Webview

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"github.com/webview/webview"
 	"net/http"
 	configUtils "stock_scraper/internal/config"
 	"stock_scraper/types"
@@ -27,8 +29,18 @@ func init() {
 	parallelChan = make(chan bool, maxConcurrency)
 }
 
+func loadWebview(addr string) {
+	w := webview.New(true)
+	defer w.Destroy()
+	w.SetTitle("Stock scraper")
+	w.SetSize(1280, 1024, webview.HintNone)
+	w.Navigate(addr)
+	w.Run()
+}
+
 func main() {
 	serverOpt := flag.Bool("s", false, "Enable the server")
+	uiOpt := flag.Bool("ui", false, "Enable the ui")
 	verboseOpt := flag.Bool("v", false, "Verbose")
 	watchOpt := flag.Bool("w", false, "Watch for changes (Not required if the server is enabled)")
 	configFile := flag.String("f", "config.json", "Config file")
@@ -48,7 +60,13 @@ func main() {
 
 		router, _ := setupRouter()
 
-		router.Run(":5000")
+		addr := "localhost:5000"
+		if *uiOpt {
+			go router.Run(addr)
+			loadWebview(fmt.Sprintf("http://%s", addr))
+		} else {
+			router.Run(addr)
+		}
 	} else {
 		if *watchOpt {
 			setupCron()
